29c54b78739128f2f4a0da7b9e78a57ddbbed07d
Brice Blanch <bricerb@gmail.com>
2018-02-01 12:28:46 -0500

    init

---
+++ src/main/java/Regression.java
@@ -0,0 +1,57 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+public class Regression {
+
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("platformVersion", "8.1");
+        desiredCapabilities.setCapability("app", "/Volumes/UUI/Projects/MyApplication2/app/build/outputs/apk/debug/app-debug.apk");
+        desiredCapabilities.setCapability("deviceName", "Android Emulator");
+
+        URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void sampleTest() {
+        MobileElement el1 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/next_view_btn");
+        el1.click();
+        MobileElement text1 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/passed_text_tv");
+        assertEquals(text1.getText(), "pass me");
+        MobileElement el2 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/previous_view_btn");
+        el2.click();
+    }
+
+    @Test
+    public void sampleTest2() {
+        MobileElement el1 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/edit_text_et");
+        el1.sendKeys("defaulted");
+        MobileElement el2 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/next_view_btn");
+        driver.hideKeyboard();
+        el2.click();
+        MobileElement text1 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/passed_text_tv");
+        assertEquals(text1.getText(), "defaulted");
+        MobileElement el3 = (MobileElement) driver.findElementById("com.example.rrblanch_incomm.myapplication:id/previous_view_btn");
+        el3.click();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
