1d676b73efd506b0a3dd96ada8a78521e4b4b89a
Brice Blanch <bricerb@gmail.com>
2018-02-06 09:54:49 -0500

    detecting environment for correct build information

--- src/test/java/mobile/android/TestCapabilities.java
+++ src/test/java/mobile/android/TestCapabilities.java
@@ -1,15 +1,20 @@
 package mobile.android;
 
 public class TestCapabilities {
+
+    private static boolean isBitriseBuild = (System.getenv().get("REGRESSION_TEST_GIT_URL") != null);
+    public static final String APP = isBitriseBuild ? "/bitrise/deploy/app-debug.apk" : "/Users/rrblanch-incomm/Incomm/Android-WL-Pay-it-Here/app/build/outputs/apk/debug/app-debug.apk";
+    public static final String PLATFORM_VERSION = isBitriseBuild ? "8.0" : "8.1";
 //    Bitrise URL and Version
-//    public static final String APP_URL = "/bitrise/deploy/app-debug.apk";
+//    public static final String APP = "/bitrise/deploy/app-debug.apk";
 //    public static final String PLATFORM_VERSION = "8.0";
 
 //    Local URL and Version
-    public static final String APP = "/Users/rrblanch-incomm/Incomm/Android-WL-Pay-it-Here/app/build/outputs/apk/debug/app-debug.apk";
-    public static final String PLATFORM_VERSION = "8.1";
+//    public static final String APP = "/Users/rrblanch-incomm/Incomm/Android-WL-Pay-it-Here/app/build/outputs/apk/debug/app-debug.apk";
+//    public static final String PLATFORM_VERSION = "8.1";
 
     public static final String PLATFORM_NAME = "Android";
     public static final String DEVICE_NAME = "Android Emulator";
     public static final String END_POINT = "http://localhost:4723/wd/hub";
 }
+
